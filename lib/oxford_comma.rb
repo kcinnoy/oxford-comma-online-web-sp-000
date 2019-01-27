cb86d810e019379213972b07d3d8b221a73a34b1
Yonnick Brown <25hour24@gmail.com>
2019-01-27 20:08:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,8 @@
 def oxford_comma(array)
-  array.join
-  array.join(' and ')
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(' and ')
+  else
   "#{array[0...-1].join(', ')}, and #{array[-1]}"
 end
